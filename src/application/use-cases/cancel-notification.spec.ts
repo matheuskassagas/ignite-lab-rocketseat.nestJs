1d8e441e75ff453833bf03cb1d664422fecb7cf9
matheuskassagas <gigliotti_@hotmail.com>
2022-12-21 11:23:27 -0300

    tests tested

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,6 +2,7 @@ import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { ErrorNotFound } from './errors/not-found';
 
 describe('Cancel notification', () => {
   it('should be able to send a notification', async () => {
@@ -22,5 +23,16 @@ describe('Cancel notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(ErrorNotFound);
   });
 });
